da8da0b316bfe6f390a076fb3c419a346ade4413
rajkumarkashyap3582 <rajkumarkashyap3582@gmail.com>
2024-06-15 12:52:20 +0530

    image set

--- resources/views/layouts/upnl/footer.blade.php
+++ resources/views/layouts/upnl/footer.blade.php
@@ -23,9 +23,8 @@
                         </div>
                         <div data-v-6049d237="" class="item">
                             <div data-v-6049d237="" class="">
-                                <div data-v-6049d237="" class="icon"><img data-v-6049d237=""
-                                        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwBAMAAAClLOS0AAAAJ1BMVEUAAAD///////////////////////////////////////////////+uPUo5AAAADXRSTlMAgHgQYD9wSCAIUDBoFctLawAAAL1JREFUOMtjoBVgjjllgE2cw1FQMBmHuKA4pjg3SFxQDEN8qiJQ2FVQEKu4BwOGhDlEHEOiBSgsVMwAksAUj2TAkNgCEceQaAOKi2xgwJBgFUQDKlBFhoLoQAwSYAsFMUAmWOIgpoQo1L7FqJ6yEhSBShigxYqgEKbToSKDTIJVqQC7RKGgKHYJRUEhEiUCBcWxS7ArBQzCIEEWUcQVtY64EkMgZvKRBktMw5RIgBiZiCOJMnA7oSXqSAb6AADSxiaTuz9TTgAAAABJRU5ErkJggg=="
-                                        alt=""></div><a href="{{route('user.Withdraw')}}"><span data-v-6049d237="">Withdraw</span>
+                                <div data-v-6049d237="" class="icon"><a href="{{route('user.profile')}}"><img data-v-6049d237=""
+                                        src="{{ asset('') }}assets/static/img/wallet.png"  alt=""></a></div><span data-v-6049d237="">Profile</span>
                             </div>
                         </div>
                         <div data-v-6049d237="" class="item">
