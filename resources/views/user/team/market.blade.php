56742ad234c94134fe0b509cdf790537782bee5c
ramesh-kashyap <75538046+ramesh-kashyap@users.noreply.github.com>
2024-06-20 19:27:20 +0530

    22

--- resources/views/user/team/market.blade.php
+++ resources/views/user/team/market.blade.php
@@ -208,8 +208,8 @@
                 </div>
             </div>
 
-            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-            <script src="{{asset('')}}assets/js/crypto.js"></script>
+            {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
+            {{-- <script src="{{asset('')}}assets/js/crypto.js"></script> --}}
 
 
             @include('layouts.upnl.footer')
